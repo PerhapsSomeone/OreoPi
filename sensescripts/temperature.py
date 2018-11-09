5fc1fd26dfb5d5525f96ec5db021b090cc99068c
PerhapsSomeone <ma2003br@gmail.com>
2018-11-09 20:30:42 +0000

    Fixed threading

--- sensescripts/temperature.py
+++ sensescripts/temperature.py
@@ -6,12 +6,5 @@ from time import sleep
 
 sense = SenseHat()
 
-def threaded_function(arg):
-    temperature = sense.temperature
-    sense.show_message(str(temperature))
-
-
 temperature = sense.temperature
-thread = Thread(target = threaded_function)
-thread.start()
 print(str(temperature))
