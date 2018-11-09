707d201c05d44a079f58bd0e815be9bdfa031b4f
PerhapsSomeone <ma2003br@gmail.com>
2018-11-09 18:21:35 +0100

    Fixed sensescript

--- sensescripts/temperature.py
+++ sensescripts/temperature.py
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 from sense_hat import SenseHat
+from threading import Thread
+from time import sleep
 
 sense = SenseHat()
 
+def threaded_function(arg):
+    temperature = sense.temperature
+    sense.show_message(str(temperature))
+
+
 temperature = sense.temperature
-sense.show_message(str(temperature))
+thread = Thread(target = threaded_function)
+thread.start()
 print(str(temperature))
